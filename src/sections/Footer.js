7183301e1bcdd5deafcc7dfcd6a586c89371706c
angelgr-com <dev@angelgr.com>
2022-05-08 20:21:14 +0200

    feat: footer draft

--- src/sections/Footer.js
+++ src/sections/Footer.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Footer = () => {
+  const now = new Date();
+
   return (
-    <div>Footer</div>
+    <Footers>© {now.getFullYear()} Naulan</Footers>
   )
 }
+
+const Footers = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem 5rem;
+  background-color: var(--gd2);
+  color: var(--gl2);
+  height: 6em;
+`;
 
 export default Footer;
