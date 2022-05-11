5422d363b3408997a182e4dc1021b003a946a0ff
angelgr-com <dev@angelgr.com>
2022-05-11 15:40:18 +0200

    feat: use redux in Header

--- src/sections/Header.js
+++ src/sections/Header.js
@@ -1,21 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { LOGOUT, MOVIES_TITLE } from '../store/types';
+import { connect } from 'react-redux';
+import axios from 'axios';
 import styled from 'styled-components';
 import { FaUserAlt } from 'react-icons/fa';
 import { GrMenu } from 'react-icons/gr';
 
-const Header = () => {
-  return (
-    <Headers>
-      <Container>
-        <Logos href="/">Naulan</Logos>
-        <Navs>
-          <a href="/register">Register</a>
-          <Buttons href="/login"><FaUserAlt /></Buttons>
-          <Buttons href="/"><GrMenu /></Buttons>
-        </Navs>
-      </Container>
-    </Headers>
-  )
+const Header = (props) => {
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    console.log('Redux credentials: ', props.credentials);
+  });
+
+  const redirect = (route) => {
+    setTimeout(() => {
+      navigate(route);
+    }, 200);
+  }
+
+  const logOut = () => {
+    // Delete credentials from redux
+    props.dispatch({ type: LOGOUT });
+
+    setTimeout(() => {
+      navigate("/");
+    }, 1000);
+  }
+
+  // Render without credentials
+  if (!props.credentials?.token) {
+    return (
+      <Headers>
+        <Container>
+          <Logos href="/">Naulan</Logos>
+          <Navs>
+            <a href="/register">Register</a>
+            <Buttons href="/login"><FaUserAlt /></Buttons>
+            <Buttons href="/"><GrMenu /></Buttons>
+          </Navs>
+        </Container>
+      </Headers>
+    )
+  } else {
+    return (
+      <Headers>
+        <Container>
+          <Logos href="/">Naulan</Logos>
+          <Navs>
+            <a href="/logout">Logout</a>
+            <Buttons href="/login"><FaUserAlt /></Buttons>
+            <Buttons href="/"><GrMenu /></Buttons>
+          </Navs>
+        </Container>
+      </Headers>
+    )
+  }
 }
 
 const Headers = styled.header`
