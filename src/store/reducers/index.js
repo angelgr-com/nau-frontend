0d3903854ae8de2ac09172b48a0635e25b4db535
angelgr-com <dev@angelgr.com>
2022-05-11 15:13:52 +0200

    feat: add redux files configuration

---
+++ src/store/reducers/index.js
@@ -0,0 +1,9 @@
+import {combineReducers} from 'redux';
+
+import credentials from './datosLogin-reducer';
+
+const rootReducer = combineReducers({
+    credentials,
+});
+
+export default rootReducer;
