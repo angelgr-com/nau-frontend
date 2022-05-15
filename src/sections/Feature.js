5511492af51bfb57c18643d49b11aeefd38049fd
angelgr-com <dev@angelgr.com>
2022-05-15 22:54:16 +0200

    fix: added some text to home translation cards

--- src/sections/Feature.js
+++ src/sections/Feature.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import TranslationCard from '../sections/TranslationCard';
+import TextTop from '../components/TextTop';
+import TranslationCardBottom from '../components/TranslationCardBottom';
+
 
 const Feature = (props) => {
   return (
@@ -9,10 +11,40 @@ const Feature = (props) => {
         <Header>{props.header}</Header>
         <Paragraph>{props.paragraph}</Paragraph>
       </Description>
-      <TranslationCard />
+      <TranslationCardSt>
+        <TextTop
+          text='The merit of all things lies in their difficulty.'
+        />
+        <TranslationCardBottom />
+      </TranslationCardSt>
     </Features>
   )
 };
+
+const TranslationCardSt = styled.div`
+  align-items: center;
+  border-radius: 2em;
+  border: 0.15em solid var(--gl1);
+  box-shadow: 0.2em 0.2em 0.6em 0.1em rgba(0, 0, 0, 0.2);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 1em 2em 1em 1em;
+  min-height: 20em;
+  min-width: 24em;
+  padding: 2em;
+  @media only Screen and (max-width: 60em) {
+    min-width: 18em;
+  }
+  @media only Screen and (max-width: 48em) {
+    min-width: 20em;
+    margin: 1em 1em 1em 1em;
+  }
+  @media only Screen and (max-width: 30em) {
+    min-width: 14em;
+    margin: 1em 1em 1em 1em;
+  }
+`;
 
 const Features = styled.div`
   box-shadow: 0.2em 0.2em 0.6em 0.1em rgba(0, 0, 0, 0.2);
